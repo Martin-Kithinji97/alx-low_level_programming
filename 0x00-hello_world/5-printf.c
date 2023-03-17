601d20a43044f3c51826c709878d2a92f2789512
Martin-Kithinji97 <mmurithi18@gmail.com>
2023-03-17 03:29:07 -0700

    werror

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  *main - prints out the sentense
+  *
+  *Return: Always 0 if command is succesful
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
