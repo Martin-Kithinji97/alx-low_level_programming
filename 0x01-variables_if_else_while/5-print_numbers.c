891c0cf01f3822dc4dbdc8724edc0af95cd50f78
Martin-Kithinji97 <mmurithi18@gmail.com>
2023-03-18 13:48:17 -0700

    printing to base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - its the main function
+  * description - prints numbers to base 10
+  * Return: always 0 if successful
+  */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
