98a7880ff25a7b5240153b68506b5351d98ab2fe
Martin-Kithinji97 <mmurithi18@gmail.com>
2023-03-22 05:37:12 -0700

    add sum of numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - the function adds two numbers
+ * @a: parametre 1
+ * @b: parametre 2
+ * Return: return sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+
