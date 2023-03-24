068a52787b616c7434abc121b92e751e315cbd4b
Martin-Kithinji97 <mmurithi18@gmail.com>
2023-03-24 01:13:22 -0700

    prints line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * print_line - the function prints a line
+  * Return: it returns 0 when successful
+  */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('_');
+		_putchar('\n');
+	}
+	_putchar('_');
+}
