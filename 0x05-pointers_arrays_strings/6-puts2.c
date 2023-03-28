197fb7b565eb5a8f025a7ccf16a46a342ca7cb5b
Martin-Kithinji97 <mmurithi18@gmail.com>
2023-03-28 12:48:21 -0700

    prints even

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,28 @@
   */
 void puts2(char *str)
 {
-	int i;
+	int longi = 0;
+	int t = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
 	}
 	_putchar('\n');
 }
+
+
+
